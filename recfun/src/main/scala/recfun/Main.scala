4c158686b1b189aa0951d6f27c12add7492a67c0
YJ <lala7573@gmail.com>
2014-02-24 04:04:16 +0900

    arrang assigment week1

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -3,24 +3,16 @@ import common._
 
 object Main {
 	def main(args: Array[String]) {
-		println("countChange")
-		println(countChange(300,List(500,5,50,100,20,200,10)))
-
-		//		println("Pascal's Triangle")
-		//		for (row <- 0 to 10) {
-		//			for (col <- 0 to row)
-		//				print(pascal(col, row) + " ")
-		//				println()
-		//		}
+		println("Pascal's Triangle")
+		for (row <- 0 to 10) {
+			for (col <- 0 to row)
+				print(pascal(col, row) + " ")
+				println()
+		}
 	}
 
 	/**
 	 * Exercise 1
-	 * r = 0 : 1
-	 * r = 1 : 1 1
-	 * f = 2 : 1 2 1
-	 * r = 3 : 1 3 3 1
-	 * r = 4 : 1 4 6 4 1
 	 */
 	def pascal(c: Int, r: Int): Int = {
 			if ( r == 0 || c == 0 || r == c) 1
